925a621d7af7e6f08ec8339b4d16d25c1011b5c8
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-16 03:08:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,11 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
     SQL
+
+    DB[:conn].execute(sql, name)
   end
 end
