48da3250c1cec03c1e37b1d745c4f014c3c04c60
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-16 03:02:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
 
   def self.new_from_db(row)
     student = self.new
-    student.id = row[0]
+    student.id = row[:id]
     student.name = row[1]
     student.grade = row[2]
     student
